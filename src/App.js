a141018722f1b4fc2ec9986b8c40b8450bd0f516
shreyas patil <shreyaspatil1901@gmail.com>
2023-04-10 02:16:16 +0530

    done with the scroll bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import "./App.css";
-import Card from "react-bootstrap/Card";
-import Users from "./Users";
-import "./index.css";
 
-import Navbar from "./components/navbar";
+const style = {
+  border: "1px solid red",
+  margin: 12,
+  padding: 8,
+};
 
 function App() {
+  const [dataSource, setdataSource] = useState(Array.from({ length: 20 }));
+  const [hasMore, sethasMore] = useState(true);
+  const fetchMoreData = () => {
+    if (dataSource.length < 200) {
+      setTimeout(() => {
+        setdataSource(dataSource.concat(Array.from({ length: 20 })));
+      }, 500);
+    } else {
+      sethasMore(false);
+    }
+  };
   return (
-    <div>
-      <section className="App">
-        <Navbar />
-        <div className="App">
-          <div>ADD SOMETHING TO THE LIST</div>
-          <input type="text" />
-        </div>
-        <div className="Card">
-          <Users />;
-        </div>
-      </section>
+    <div className="App">
+      <p>scrollBar</p>
+      <InfiniteScroll
+        dataLength={dataSource.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader=<p>loading...</p>
+        endMessage={<p>You are all set</p>}
+        height={200}
+      >
+        {dataSource.map((item, index) => {
+          return <div style={style}>This is a div #{index + 1} card</div>;
+        })}
+      </InfiniteScroll>
     </div>
   );
 }
