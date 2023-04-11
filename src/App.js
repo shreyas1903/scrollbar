5ee22881a6806ddfbac9c757b8acd8d9cf2bc7ed
shreyas patil <shreyaspatil1901@gmail.com>
2023-04-11 18:06:50 +0530

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 import "./App.css";
 
